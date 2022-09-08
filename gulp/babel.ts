a60f8bea42045af56356f6f1fe023ac1317a2c2b
Phanabani <phanabani@gmail.com>
2022-09-08 16:15:00 -0700

    Remove gulp implementation.

--- gulp/babel.ts
+++
@@ -1,8 +0,0 @@
-import { dest, src } from 'gulp';
-import gulpBabel from 'gulp-babel';
-
-export const babel = () => {
-  src('src/**/*')
-    .pipe(gulpBabel())
-    .pipe(dest('dist'));
-};
--- gulpfile.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Author: Alexandre Havrileck (Oxyno-zeta)
- * Date: 03/07/16
- * Licence: See Readme
- */
-
-export * from './gulp';
